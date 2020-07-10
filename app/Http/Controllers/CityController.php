f9023ab33aa538e0452cc12fb8d2e7b5c97318af
Jorge Wong <jorgewong98@gmail.com>
2020-07-10 16:44:45 -0700

    Tres metodos de la API de Ciudades

---
+++ app/Http/Controllers/CityController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\City;
+use App\Registry;
+
+class CityController extends Controller
+{
+    function getAll()
+    {
+        $cities =  City::all();
+        return $cities;
+    }
+
+    function find($id)
+    {
+        try {
+            $city = City::findOrFail($id);
+            return $city;
+        } catch (\Throwable $th) {
+            return response('Id de la ciudad invalido.', 400);
+        }
+    }
+
+    function getCityData(Request $request, $id)
+    {
+        try {
+            $city = City::findOrFail($id);
+            $data;
+            $lastDays = request()->query('lastDays');
+            if (!is_null($lastDays) && ctype_digit($lastDays)) {
+                $city['registries'] = Registry::where('city_id', $city->id)
+                                ->orderBy('date', 'DESC')
+                                ->take($lastDays)
+                                ->get();
+            }
+            else {
+                $city['registries'] = $city->registries;
+            }
+            return $city;
+        } catch (\Throwable $th) {
+            $message = "";
+            switch ($th->getCode()) {
+                case 0:
+                    $message = "Id de la ciudad invalido.";
+                    break;
+                default:
+                    $massage = "Codigo de error: " . $th->getCode();
+                    break;
+            }
+            return response($message, 400);
+        }
+    }
+}
